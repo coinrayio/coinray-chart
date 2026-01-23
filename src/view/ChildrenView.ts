a03059abc674af276f721d52d4bce4610722e767
Abdulbasit Mamman <basttyy@gmail.com>
2026-01-23 09:38:50 +0100

    feat(implement-heikin-ashi-bars): mutate data to candleType for heikin ashi

--- src/view/ChildrenView.ts
+++ src/view/ChildrenView.ts
@@ -24,7 +24,7 @@ export type EachChildCallback = (
 ) => void
 
 export default abstract class ChildrenView extends View<YAxis> {
-  protected eachChildren (childCallback: EachChildCallback, mutateToCandleType: boolean = false): void {
+  protected eachChildren (childCallback: EachChildCallback, mutateToCandleType = false): void {
     const pane = this.getWidget().getPane()
     const chartStore = pane.getChart().getChartStore()
     const visibleRangeDataList = chartStore.getVisibleRangeDataList(mutateToCandleType)
